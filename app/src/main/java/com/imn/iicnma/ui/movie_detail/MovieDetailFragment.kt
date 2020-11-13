2144ec4c89e9dc8fd8e485eb91a58e75ea30f4b4
iman irandoost <imn.irdst@gmail.com>
2020-11-13 22:14:20 +0330

    Add movieDetail dummy view

---
+++ app/src/main/java/com/imn/iicnma/ui/movie_detail/MovieDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.imn.iicnma.ui.movie_detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.imn.iicnma.R
+
+class MovieDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MovieDetailFragment()
+    }
+
+    private lateinit var viewModel: MovieDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MovieDetailViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/imn/iicnma/ui/movie_detail/MovieDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.imn.iicnma.ui.movie_detail
+
+import androidx.lifecycle.ViewModel
+
+class MovieDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
