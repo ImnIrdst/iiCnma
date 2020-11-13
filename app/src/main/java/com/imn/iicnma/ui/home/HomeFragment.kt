bd2069c2951a85a8912e8741721061fec0e63a14
iman irandoost <imn.irdst@gmail.com>
2020-11-13 09:14:47 +0330

    Handle empty page state

--- app/src/main/java/com/imn/iicnma/ui/home/HomeFragment.kt
+++ app/src/main/java/com/imn/iicnma/ui/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
@@ -31,14 +33,15 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater).apply {
             recyclerView.apply {
-                adapter = homeAdapter.withLoadStateFooter(
+                adapter = homeAdapter.withLoadStateHeaderAndFooter(
+                    header = ListLoadStateAdapter { homeAdapter.retry() },
                     footer = ListLoadStateAdapter { homeAdapter.retry() }
                 )
                 layoutManager = GridLayoutManager(context, 2).apply {
                     spanSizeLookup = object : SpanSizeLookup() {
                         override fun getSpanSize(position: Int): Int {
                             return when ((adapter as ConcatAdapter).getItemViewType(position)) {
-                                0 -> 1
+                                0 -> 1 // TODO make this better
                                 else -> 2
                             }
                         }
@@ -51,11 +54,28 @@ class HomeFragment : Fragment() {
     }
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launch {
             homeViewModel.movies.collectLatest { homeAdapter.submitData(it) }
+        }
+
+        loadStateLayout.retryButton.setOnClickListener { homeAdapter.retry() }
+
+        homeAdapter.addLoadStateListener { loadState ->
+
+            recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+            loadStateLayout.progressBar.isVisible =
+                loadState.source.refresh is LoadState.Loading
+
+            val errorState = loadState.source.refresh as? LoadState.Error
+
+            loadStateLayout.retryButton.isVisible = (errorState != null)
+            loadStateLayout.messageTextView.apply {
+                isVisible = (errorState != null)
+                text = errorState?.error.toString()
+            }
         }
     }
 }
--- app/src/main/java/com/imn/iicnma/ui/home/ListLoadStateAdapter.kt
+++ app/src/main/java/com/imn/iicnma/ui/home/ListLoadStateAdapter.kt
@@ -6,7 +6,7 @@ import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.imn.iicnma.databinding.LoadStateListFooterBinding
+import com.imn.iicnma.databinding.LoadStateBinding
 
 class ListLoadStateAdapter(
     private val retry: () -> Unit
@@ -20,7 +20,7 @@ class ListLoadStateAdapter(
 }
 
 class LoadStateViewHolder(
-    private val binding: LoadStateListFooterBinding,
+    private val binding: LoadStateBinding,
     retry: () -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
@@ -39,7 +39,7 @@ class LoadStateViewHolder(
 
     companion object {
         fun create(parent: ViewGroup, retry: () -> Unit): LoadStateViewHolder {
-            val binding = LoadStateListFooterBinding.inflate(
+            val binding = LoadStateBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
             )
             return LoadStateViewHolder(binding, retry)
